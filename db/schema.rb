987abcf6354506b72fb8e1846d936837ee20473a
Alex <alex@ascholtes.me>
2017-09-15 10:38:57 -0400

    finished the api_token, also started creating our entries table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170911163931) do
+ActiveRecord::Schema.define(version: 20170915143418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "entries", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.boolean "published"
+    t.bigint "quality"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -23,6 +32,8 @@ ActiveRecord::Schema.define(version: 20170911163931) do
     t.datetime "updated_at", null: false
     t.string "api_token"
     t.string "password_digest"
+    t.string "first_name"
+    t.string "last_name"
   end
 
 end
